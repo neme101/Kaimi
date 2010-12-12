b163d7f2128ae1a86fedbc8799642b6c6c33cef0
Samus_ <mail2samus@gmail.com>
2010-12-11 21:25:24 -0500

    rails base project

---
+++ kaimi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaimi::Application.config.secret_token = '152c103e8fa4e647908087088cffd52034e891a34fe51cc42d2fff6e337b97c25898102188330a2cb0c64738608a2e52c5b84bc5d614cfa81c455df50a76c11c'
